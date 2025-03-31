b070f5d2bca2cfe7186cc057d1ffda8be46a9f47
Andrew <andrewlokoffical@gmail.com>
2025-03-31 23:32:31 +0200

    Add Page and Navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
+import Image from 'next/image';
 
 const Navbar = () => {
     return (
         <nav>
             <div>
                 <div>
-                    <p></p>
+                    <Image src="/logo.png" alt='' width={50} height={50} />
+                    <p>Not Brain Found</p>
                 </div>
                 <ul>
-                    <li><a href=""></a></li>
-                    <li><a href=""></a></li>
-                    <li><a href=""></a></li>
-                    <li><a href=""></a></li>
-                    <li><a href=""></a></li>
-                    <li><a href=""></a></li>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">About</a></li>
+                    <li><a href="">Projects</a></li>
+                    <li><a href="">Services</a></li>
+                    <li><a href="">Developers</a></li>
+                    <li><a href="">Contact</a></li>
                 </ul>
                 <button>Let&apos;s connect</button>
             </div>
